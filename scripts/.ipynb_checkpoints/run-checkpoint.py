722d339ffeaad1a2c6dd5382df6f8346a5531795
Chabriem73 <chabriem73@gmail.com>
2020-10-09 12:41:28 +0200

    essai

--- scripts/.ipynb_checkpoints/run-checkpoint.py
+++ scripts/.ipynb_checkpoints/run-checkpoint.py
@@ -3,6 +3,7 @@ from implementation import *
 from helpers import *
 from findbestdegree import *
 from proj1_helpers import *
+from datetime import datetime
 
 OUT_DIR = "../out"
 
@@ -11,16 +12,17 @@ OUT_DIR = "../out"
 def main():
     print("LOAD DATA")
     ys_train, x_train, ids_train = load_csv_data("../data/train.csv")
-    x_test, ids_test = load_csv_data("../data/train.csv")
+    y_test, x_test, ids_test = load_csv_data("../data/test.csv")
 
     print("FILTERING DATA")
     x_test, x_train = standardize(x_test, x_train)
     x_train, ys_train = remove_outliers(x_train, ys_train)
 
-    print("BUILDING POLYNOMIALS")
-    degrees = np.arange(2,11);
-    lamdas = np.logspace(-4, 0, 30)
-    best_degree = best_degree_selection(degrees, 4, lambdas);
+    print("FINDING DEGREE")
+    degrees = np.arange(10,11);
+    lambdas = np.logspace(-4, 0, 2)
+    best_degree = best_degree_selection(x_train,ys_train,degrees, 2, lambdas);
+    print("BUILDING POLYNOMIALS with degree", best_degree)
     tx_train = build_poly(x_train, best_degree)
     tx_test = build_poly(x_test, best_degree)
 
