091d11037ec5b8c1aa1d94e42e949dbfab599d8a
Chabriem73 <chabriem73@gmail.com>
2020-10-20 20:39:33 +0200

    xx

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -34,8 +34,6 @@ def gradient_descent(y, tx, initial_w, max_iters, gamma):
         # store w and loss
         ws.append(w)
         losses.append(loss)
-        #print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
-         #     bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
     return losses, ws
 
 def build_poly(x, degree):
